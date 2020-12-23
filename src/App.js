e77a83560d961cf26913289cbdcdfa0dfb62fe3f
Mohammad Akram <akramaltaf786@gmail.com>
2020-12-24 00:14:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Banner from "./components/Banner";
+import Collections from "./components/Collections";
+import Footer from "./components/Footer";
+import Gallery from "./components/Gallery";
+import MobileHeader from "./components/MobileHeader";
+import MobileNav from "./components/MobileNav";
+import Navbar from "./components/Navbar";
+import Subscribe from "./components/Subscribe";
+import Testimonial from "./components/Testimonial";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <MobileHeader />
+      <MobileNav />
+      <Banner />
+      <Gallery />
+      <Collections />
+      <Testimonial />
+      <Subscribe />
+      <Footer />
     </div>
   );
 }
